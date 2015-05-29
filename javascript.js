3279f540d431b5d7c5c3d44256f88e2c2f626efe
Louie <dextroyer026@gmail.com>
2015-05-29 19:20:05 +0800

    ewqe

--- javascript.js
+++ javascript.js
@@ -140,6 +140,8 @@ function viewPathHistory(){
 			};
 			//centering the path
 			map.fitBounds(latlonBounds);
+			marker = new google.maps.Marker ( { position : path[0], map : map } );
+
 		}
 	);
 }

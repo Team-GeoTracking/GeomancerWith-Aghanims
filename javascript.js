75b9730f1e5fe5e144d8ca34b7c2e6324f46d268
Louie <dextroyer026@gmail.com>
2015-06-01 15:59:36 +0800

    added endpoint on save and view

--- javascript.js
+++ javascript.js
@@ -4,6 +4,7 @@ var errormsg = $('p#errormsg');
 var gmap = navigator.geolocation;
 var path = [];
 var mapholder = document.getElementById('mapholder');
+var mapa;
 
 
 //watch position button
@@ -42,7 +43,6 @@ function showPosition(position) {
     //display coordinates
 	document.getElementById('coordinates').innerHTML = path;
 }
-
 function addPath() {
 
 	var lat = 14.5861074;
@@ -51,19 +51,19 @@ function addPath() {
 
 	var myOptions = {
 		center : latlon,
-		zoom : 12,
+		zoom : 14,
 		mapTypeId : google.maps.MapTypeId.ROADMAP
 	}
 
-	var map = new google.maps.Map(mapholder,myOptions);
+	mapa = new google.maps.Map(mapholder,myOptions);
 
-	google.maps.event.addListener(map, "click", function (e) {
+	google.maps.event.addListener(mapa, "click", function (e) {
     //lat and lng is available in e object
     var latLng = e.latLng;
     path.push(latLng);
-    marker = new google.maps.Marker ( { position : path[0], map : map } );
+    marker = new google.maps.Marker ( { position : path[0], map : mapa, icon : "startIcon.png", title : "Start Point" } );
     document.getElementById('coordinates').innerHTML = path;
-    var polyline = new google.maps.Polyline({ map: map, path: path, strokeColor: '#0000FF', strokeOpacity: 0.7, strokeWeight: 4});});
+    var polyline = new google.maps.Polyline({ map: mapa, path: path, strokeColor: '#0000FF', strokeOpacity: 0.7, strokeWeight: 4});});
 
 }
 
@@ -85,6 +85,8 @@ function savePath(){
 		//alert(data);
 		errormsg.html("Done!")
 	})
+	var lastLocation = path.length-1;
+	marker = new google.maps.Marker ( { position : path[lastLocation], map : mapa, title : "End Point"} );
 }
 
 function viewPathHistory(){
@@ -93,7 +95,7 @@ function viewPathHistory(){
 	var name = $('input#username').val();
 	$.post('viewpathhistory.php',{name : name},
 		function (data) {
-
+			var pathHistory = [];
 
 			console.log(data);
 			console.log(typeof data);
@@ -116,11 +118,11 @@ function viewPathHistory(){
 			//pushing the latintArray and lonintArray to path Array
 			for (var i = 0; i < latintArray.length; i++) {
 				var x = new google.maps.LatLng( latintArray[i] , lonintArray[i] );
-				path.push(x);
+				pathHistory.push(x);
 			};
 
 			var myOptions = {
-				center : path[0],
+				center : pathHistory[0],
 				zoom : 12,
 				mapTypeId : google.maps.MapTypeId.ROADMAP
 			}
@@ -128,20 +130,26 @@ function viewPathHistory(){
 			var map = new google.maps.Map(mapholder,myOptions);
 			//marker = new google.maps.Marker ( { position : x, map : map } );
 
-			var polyline = new google.maps.Polyline({ map: map, path: path, strokeColor: '#0000FF', strokeOpacity: 0.7, strokeWeight: 4});
+			var polyline = new google.maps.Polyline({ map: map, path: pathHistory, strokeColor: '#0000FF', strokeOpacity: 0.7, strokeWeight: 4});
 
 
 			// fit path on map
 
 			//make the points visible on current map
 			var latlonBounds = new google.maps.LatLngBounds();
-			for (var i = 0; i < path.length ; i++) {
-				latlonBounds.extend(path[i]);
+			for (var i = 0; i < pathHistory.length ; i++) {
+				latlonBounds.extend(pathHistory[i]);
 			};
 			//centering the path
 			map.fitBounds(latlonBounds);
-			marker = new google.maps.Marker ( { position : path[0], map : map } );
 
+			//adding start point
+			marker = new google.maps.Marker ( { position : pathHistory[0], map : map, icon : "startIcon.png", title : "Start Point" } );
+
+			//adding last point
+			var lastLocation = (pathHistory.length-1);
+			marker = new google.maps.Marker ( { position : pathHistory[lastLocation], map : map, title : "End Point" } );			
+			pathHistory = [];
 		}
 	);
 }
